0354ffffbf47632a98470c76d44da987fa08c96c
Yasuhiro Horimoto <iddqsar888plekww@gmail.com>
2018-11-26 22:23:08 +0900

    Support host and port options

--- luatika/tika.lua
+++ luatika/tika.lua
@@ -1,3 +1,5 @@
+local Client = require("luatika/client")
+
 local Tika = {}
 
 local methods = {}
@@ -7,8 +9,19 @@ function metatable.__index(tika, key)
     return methods[key]
 end
 
+local DEFAULT_HOST = "127.0.0.1"
+local DEFAULT_PORT = "9998"
+
+local function apply_options(tika, options)
+    local options = options
+    local host = options.host or DEFAULT_HOST
+    local port = options.port or DEFAULT_PORT
+    tika.client = Client.new(host, port)
+end
+
 function Tika.new(options)
     local tika = {}
+    apply_options(tika, options)
     setmetatable(tika, metatable)
     return tika
 end
