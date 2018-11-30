423ffed9dcb016ee74a4494b169c2aa12b39523b
Yasuhiro Horimoto <iddqsar888plekww@gmail.com>
2018-11-30 22:27:28 +0900

    Add a method for spawn

--- luatika/server.lua
+++ luatika/server.lua
@@ -1,3 +1,5 @@
+local Process = require("luatika/process")
+
 local Server = {}
 
 local methods = {}
@@ -7,13 +9,19 @@ function metatable.__index(server, key)
     return methods[key]
 end
 
+function methods:spawn()
+    self.process:spawn()
+end
+
 function methods:start()
     self:spawn()
     self:ensure_running()
 end
 
 function Server.new()
-    local server = {}
+    local server = {
+        process = Process.new("tika-server", "1.19.1")
+    }
     setmetatabel(server, metatable)
     return server
 end
