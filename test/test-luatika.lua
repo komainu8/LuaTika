52e6e622c8b67c794cee0bc8e3e9e4615479b741
Yasuhiro Horimoto <iddqsar888plekww@gmail.com>
2018-12-15 18:27:52 +0900

    test: Modify version test

--- test/test-luatika.lua
+++ test/test-luatika.lua
@@ -1,8 +1,12 @@
 local luaunit = require("luaunit")
 local luatika = require("luatika")
+local tika = luatika.tika.new()
 
 TestLuaTika = {}
 
 function TestLuaTika:test_version()
-    luaunit.assert_equals(luatika.VERSION, "0.0.1")
+  local callback = function(session)
+    luaunit.assert_equals(session:version(), "1.19.1")
+  end
+  tika:start_session(callback)
 end
