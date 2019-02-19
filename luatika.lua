6fef5b9436e50e64bfc8908d54913c9a182fdc9b
Yasuhiro Horimoto <iddqsar888plekww@gmail.com>
2019-02-19 20:55:57 +0900

    Restructure the main class

--- luatika.lua
+++ luatika.lua
@@ -1,6 +1,22 @@
 local luatika = {}
 
-luatika.tika = require("luatika/tika")
-luatika.VERSION = "0.0.1"
+local metatable = {}
+local method = {}
+
+local VERSION = "0.0.1"
+
+local TIKA_PATH = ""
+local DEFAULT_HOST = "127.0.0.1"
+local DEFAULT_PORT = "9998"
+
+function metatable._index(tika, key)
+    return method[key]
+end
+
+function luatika.new()
+  local tika = {}
+  setmetatable(tika, metatable)
+  return tika
+end
 
 return luatika
