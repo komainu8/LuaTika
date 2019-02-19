2636966b7775653c8b0d0cece90315a67b4a1938
Yasuhiro Horimoto <iddqsar888plekww@gmail.com>
2019-02-19 21:04:44 +0900

    Support options

--- luatika.lua
+++ luatika.lua
@@ -5,7 +5,7 @@ local method = {}
 
 local VERSION = "0.0.1"
 
-local TIKA_PATH = ""
+local TIKA_PATH = os.getenv("APACHE_TIKA_SERVER_PATH")
 local DEFAULT_HOST = "127.0.0.1"
 local DEFAULT_PORT = "9998"
 
@@ -13,8 +13,17 @@ function metatable._index(tika, key)
     return method[key]
 end
 
-function luatika.new()
+local function apply_options(tika, options)
+  local options = options or {}
+  local host = options.host or DEFAULT_HOST
+  local port = options.port or DEFAULT_PORT
+
+  local path = options.path or TIKA_PATH
+end
+
+function luatika.new(options)
   local tika = {}
+  apply_options(tika, options)
   setmetatable(tika, metatable)
   return tika
 end
