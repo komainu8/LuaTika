25a0cb22fe0726c96d20b9911c0af7caf41f9fe5
Yasuhiro Horimoto <iddqsar888plekww@gmail.com>
2019-02-20 21:12:36 +0900

    Add a send process

--- client.lua
+++ client.lua
@@ -1,10 +1,24 @@
 local Client = {}
 
+local curl = require("cURL")
 local metatable = {}
 local methods = {}
 
 function metatable.__index(client, key)
   return methods[key]
+end
+
+local function send_request(url)
+  send = culr.easy{
+    url = url,
+    [curl.OPT_VERBOSE] = true,
+  }
+  send:perform()
+  send:close()
+end
+
+function send(command)
+  send_request(command)
 end
 
 function Client.new(host, port)
