ac1252b28640f2395970323765b2a7296a262f84
Yasuhiro Horimoto <iddqsar888plekww@gmail.com>
2019-03-08 23:13:33 +0900

    Add a test for HELLO command

--- test/test-luatika.lua
+++ test/test-luatika.lua
@@ -9,6 +9,11 @@ function TestLuaTika:test_get_version_of_luatika()
   luaunit.assert_equals(tika:get_version(), "0.0.1")
 end
 
+function TestLuaTika:test_command_hello()
+  tika:start_server()
+  luaunit.assert_equals(tika:send_request("HELLO"), "0.0.1")
+  tika.stop_server()
+end
 -- function TestLuaTika:test_version()
 --   local callback = function(session)
 --     luaunit.assert_equals(session:version(), "1.19.1")
